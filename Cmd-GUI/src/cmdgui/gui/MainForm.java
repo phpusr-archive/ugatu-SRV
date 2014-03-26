5f66d37daf089002c4450a04a818894d2206f03e
phpusr <phpusr@gmail.com>
2014-03-26 17:25:53 +0600

    add: Вывод команды

--- Cmd-GUI/src/cmdgui/gui/MainForm.java
+++ Cmd-GUI/src/cmdgui/gui/MainForm.java
@@ -1,6 +1,13 @@
 package cmdgui.gui;
 
+import cmdgui.prgrunner.ProgramRunner;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author phpusr
@@ -8,18 +15,56 @@ import javax.swing.*;
  *         Time: 13:49
  */
 public class MainForm extends JFrame {
-    private JButton OKGoogleButton;
     private JPanel panel1;
+    private JList outList;
+    private JTextField cmdLine;
+    private JButton runButton;
 
     public MainForm() {
         super("Main Form");
+
         setContentPane(panel1);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        pack();
+        //pack();
+        setSize(600, 400);
 
         // Actions...
+        addActions();
 
         setVisible(true);
     }
 
+    // Обработчики событий
+    void addActions() {
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String program = cmdLine.getText();
+                String[] strings = program.split(" ");
+                List<String> list = new LinkedList<String>(Arrays.asList(strings));
+
+                String prg = list.remove(0);
+                ProgramRunner programRunner = new ProgramRunner(prg, list);
+                programRunner.run();
+                List<String> outStringList = programRunner.getOutStringList();
+
+                DefaultListModel model = (DefaultListModel) outList.getModel();
+                model.clear();
+
+                for (String out : outStringList) {
+                    model.addElement(out);
+                }
+
+                // TODO допилить
+            }
+        });
+    }
+
+    // Ручное создание компонентов
+    private void createUIComponents() {
+        System.out.println(">> createUIComponents");
+
+        DefaultListModel listModel = new DefaultListModel();
+        outList = new JList(listModel);
+    }
 }
