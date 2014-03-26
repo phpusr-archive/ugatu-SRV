59fede52e99bf054b412501264fea1c05400ea06
phpusr <phpusr@gmail.com>
2014-03-26 21:55:28 +0600

    improve: Рефакторинг Main

--- Cmd-GUI/src/cmdgui/run/Main.java
+++ Cmd-GUI/src/cmdgui/run/Main.java
@@ -2,13 +2,41 @@ package cmdgui.run;
 
 import cmdgui.gui.MainForm;
 
+import javax.swing.*;
+
 /**
  * @author phpusr
  *         Date: 26.03.14
  *         Time: 14:50
  */
+
+/**
+ * Запускающий класс
+ */
 public class Main {
+
+    /** Запуск программы */
     public static void main(String[] args) {
+        // Установка темы оформления
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+
+        // Создание и запуск главной формы
         new MainForm();
+    }
+
+    /** Выводит установленные LookAndFeels */
+    private static void printInstalledLookAndFeels() {
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            System.out.println(info.getName() + ": " + info.getClassName());
+        }
+    }
+
+    /** Системный LookAndFeel */
+    private static String getSystemLookAndFeel() {
+        return UIManager.getSystemLookAndFeelClassName();
     }
 }
