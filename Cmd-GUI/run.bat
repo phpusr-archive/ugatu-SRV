e94b3b769ae3eb92b7a7bb9b1105d7326be34afa
phpusr <phpusr@gmail.com>
2014-03-27 19:32:35 +0600

    update: run.bat

--- Cmd-GUI/run.bat
+++ Cmd-GUI/run.bat
@@ -5,6 +5,7 @@ cd ..\..\..\scripts
 call env.bat
 cd /d "%CUR_PRJ%"
 
-java -jar build\classes\artifacts\Cmd_GUI_jar\Cmd-GUI.jar
+REM java -jar build\classes\artifacts\Cmd_GUI_jar\Cmd-GUI.jar
+java -jar build\libs\Cmd-GUI.jar
 
 pause
